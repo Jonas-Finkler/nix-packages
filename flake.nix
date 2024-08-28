7793d6737b1b2900a0c54af54967b756a916d74f
Jonas <git@finkler.me>
2024-08-28 15:52:59 +0200

    Restructure flake output

--- flake.nix
+++ flake.nix
@@ -6,81 +6,77 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {nixpkgs, flake-utils, ... }: let
+    myPackages = pkgs: {
+      fhiaims = pkgs.callPackage ./packages/fhiaims {};
+      runner = pkgs.callPackage ./packages/runner {};
+      kim-api = pkgs.callPackage ./packages/kim-api {};
+      mrcpp = pkgs.callPackage ./packages/mrcpp {};
+      xcfun = pkgs.callPackage ./packages/xcfun {};
+      mrchem = pkgs.callPackage ./packages/mrchem {};
+    };
 
-        myPackages = pkgs: {
-          fhiaims = pkgs.callPackage ./packages/fhiaims {};
-          runner = pkgs.callPackage ./packages/runner {};
-          kim-api = pkgs.callPackage ./packages/kim-api {};
-          mrcpp = pkgs.callPackage ./packages/mrcpp {};
-          xcfun = pkgs.callPackage ./packages/xcfun {};
-          mrchem = pkgs.callPackage ./packages/mrchem {};
+    myPythonPackages = python: {
+      sqnm = python.callPackage ./pythonPackages/sqnm {};
+      sirius-python-interface = python.callPackage ./pythonPackages/sirius-python-interface {};
+      ase-mh = python.callPackage ./pythonPackages/ase-mh {};
+      kimpy = python.callPackage ./pythonPackages/kimpy {};
+    };
+
+    overlays = [
+      (final: prev: 
+        # normal packages
+        (myPackages final) // {
+        # python packages
+        python311 = prev.python311.override {
+          packageOverrides = python-self: python-super: (myPythonPackages python-self);
         };
-
-        myPythonPackages = python: {
-          sqnm = python.callPackage ./pythonPackages/sqnm {};
-          sirius-python-interface = python.callPackage ./pythonPackages/sirius-python-interface {};
-          ase-mh = python.callPackage ./pythonPackages/ase-mh {};
-          kimpy = python.callPackage ./pythonPackages/kimpy {};
+        python312 = prev.python312.override {
+          packageOverrides = python-self: python-super: (myPythonPackages python-self);
         };
+      })
+    ];
 
-        overlays = [
-          (final: prev: 
-            # normal packages
-            (myPackages pkgs) // {
-            # python packages
-            python311 = prev.python311.override {
-              packageOverrides = python-self: python-super: (myPythonPackages python-self);
-            };
-            python312 = prev.python312.override {
-              packageOverrides = python-self: python-super: (myPythonPackages python-self);
-            };
-          })
-        ];
+  in { 
+    # basic flake output
+    inherit overlays;
+  } // flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+      inherit overlays;
+    };
+  in {
+    # system specific output
+    packages = with pkgs; { 
+      inherit fhiaims; 
+      inherit runner;
+      inherit kim-api;
+      inherit mrchem;
+    };
+    # defaultPackage = xxx;
 
-        pkgs = import nixpkgs {
-          inherit system;
-          inherit overlays;
-        };
+    # for testing that everything compiles
+    devShells.default = pkgs.mkShell {
+      buildInputs = with pkgs; [ 
+        fhiaims
+        runner
+        kim-api
+        mrchem
+        (python311.withPackages (p: with p; [
+          sqnm
+          sirius-python-interface
+          ase-mh # there are some issues with the new ase version -> BE CAREFUL!
+          kimpy
+        ]))
+      ];
 
-
-      in rec {
-
-        inherit overlays;
-
-        packages = with pkgs; { 
-          inherit fhiaims; 
-          inherit runner;
-          inherit kim-api;
-          inherit mrchem;
-        };
-        # defaultPackage = xxx;
-
-        # for testing that everything compiles
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ 
-            fhiaims
-            runner
-            kim-api
-            mrchem
-            (python311.withPackages (p: with p; [
-              sqnm
-              sirius-python-interface
-              ase-mh # there are some issues with the new ase version -> BE CAREFUL!
-              kimpy
-            ]))
-          ];
-
-          shellHook = ''
-            export FLAKE="Jonas nixpkgs test shell"
-            # OMP
-            export OMP_NUM_THREADS=4
-            # back to zsh
-            exec zsh
-          '';
-        };
-      }
-    );
+      shellHook = ''
+        export FLAKE="Jonas nixpkgs test shell"
+        # OMP
+        export OMP_NUM_THREADS=4
+        # back to zsh
+        exec zsh
+      '';
+    };
+  });
 }
