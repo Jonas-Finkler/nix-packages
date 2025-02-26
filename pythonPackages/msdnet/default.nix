8b07569c4a97d1026e806e8ba5f3912f4499e939
Jonas <git@finkler.me>
2025-02-26 12:54:35 +0100

    Use source pull request fixing np.int

--- pythonPackages/msdnet/default.nix
+++ pythonPackages/msdnet/default.nix
@@ -15,11 +15,19 @@
 }: buildPythonPackage rec {
   pname = "msdnet";
   version = "2.0.0";
+  # src = fetchFromGitHub {
+  #   owner = "dmpelt";
+  #   repo = "msdnet";
+  #   rev = "v${version}";
+  #   sha256 = "sha256-KetDjiEhSrzBSbTJuoyXM0ErUkIKth2NHYJtW1BFxJs=";
+  #   # fetchSubmodules = true;
+  #   # deepClone = true;
+  # };
   src = fetchFromGitHub {
-    owner = "dmpelt";
+    owner = "Jonas-Finkler";
     repo = "msdnet";
-    rev = "v${version}";
-    sha256 = "sha256-KetDjiEhSrzBSbTJuoyXM0ErUkIKth2NHYJtW1BFxJs=";
+    rev = "75fe55db2f164eb793ab5dbdac3506bc0ff774ea";
+    sha256 = "sha256-IlP6fys3ofBagYoaBaljvyAk7eTqipK6BOG7ZnlVaJA=";
     # fetchSubmodules = true;
     # deepClone = true;
   };
